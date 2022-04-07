3a85dd90433326bda87bc0f90a90e83de902f411
RicardoSantos-Dev <rs83168364@gmail.com>
2022-04-07 20:57:49 -0300

    Atualiza JS
    Criação da função remove selecionados

---
+++ Projeto_M1.js
@@ -0,0 +1,80 @@
+// // para transformar o JSON em objeto
+// let produto = [];
+// const produtoJSON = localStorage.getItem('ListaProdutos');
+
+// produto = JSON.parse(produtoJSON);
+
+// console.log(produto);
+
+// a função de atualiza tela tem que colocar apos a recuperação do objeto JSON
+
+
+const input = document.getElementById('txt');
+const botaoAdcionar = document.getElementById('add');
+const botaoRemoveTudo = document.getElementById('remTudo');
+const botaoRemoveSelecionado = document.getElementById('remSel');
+const botaoLocalStorage = document.getElementById('local');
+const ul = document.getElementById('ul-lista');
+let listaProduto = [];
+    
+
+ function atualizarLista() {
+    ul.innerHTML = '';
+    listaProduto.forEach(item => {
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        
+        checkbox.onclick = event => {
+            if(event.target.checked){
+                item.status = true;
+                let valor = prompt('Digite o Valor Do Produto:');
+                item.valor = Number(valor);
+                console.log(item.valor);
+            } else {
+                item.status = false;
+            }
+            console.log(item.status);
+        }
+        const btn = document.createElement('button');
+        btn.innerText = 'X';
+        const li = document.createElement('li');
+        li.innerText = item.name;
+        ul.appendChild(li);
+        li.appendChild(checkbox);
+        li.appendChild(btn);
+       
+    });
+}
+
+ function adcionar(){
+    const inputProduto = input.value;
+    if(!inputProduto) return;
+    // pegarDados();
+    // validarInput();
+     listaProduto.push({
+         name: inputProduto
+     });
+
+     input.value = '';
+    atualizarLista();
+ }
+
+ atualizarLista();
+
+botaoAdcionar.addEventListener('click',adcionar);
+botaoRemoveTudo.addEventListener('click', () => {
+    ul.innerHTML = [];
+});
+botaoRemoveSelecionado.addEventListener('click', () => {
+  listaProduto.filter(item => {
+      if(item.status === true){
+          listaProduto.splice(item, 1);
+      }
+  })
+});
+botaoLocalStorage.addEventListener('click', salvarStorage => {
+    const produtoJSON = JSON.stringify(listaProduto);
+    localStorage.setItem('ListaProdutos', produtoJSON);
+});
+  
+
